23efee18537d87b30c4dcd2d539d8c4b8b72b954
radionov <boss@flagstudio.ru>
2017-08-28 14:00:10 +0500

    vars

--- wp_rehost.sh
+++ wp_rehost.sh
@@ -1,6 +1,12 @@
-mysqldump -u root -p252525 obl_2 > ./db.sql
+read -p 'название базы?:' db
+read -p 'название юзера?:' user
+read -p 'пароль?:' pass
+read -p 'какой домен сейчас?:' old_domain
+read -p 'какой домен надо?:' new_domain
+
+mysqldump -u$user -p$pass $db > ./db.sql
 echo "dump created";
-sed -i "" 's/obl.flagstudio.ru/obl.loc/g' db.sql
+sed -i "" 's/'$old_domain'/'$new_domain'/g' db.sql
 echo "domain replaced";
 mysql -u root -p252525 obl_2 < ./db.sql
 echo "dump uploaded";
