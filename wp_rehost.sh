fad3757b2f569106b2449f3027119bd04cf9a01a
radionov <boss@flagstudio.ru>
2018-08-11 17:52:52 +0500

    VERSION 2. Menu, pull from remote, showing stats and other stuff

--- wp_rehost.sh
+++ wp_rehost.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_NAME="WP_REHOST"
+
 # Colors
 L_RED='\033[1;31m'
 L_GREEN='\033[1;32m'
@@ -9,8 +11,9 @@ D_GREY='\033[1;30m'
 D_VIOL='\033[1;34m'
 NC='\033[0m'
 
+# Check previous command error status
 check_command_exec_status () {
-  if [ $1 -eq 0 ]
+  if [[ $1 -eq 0 ]]
     then
       echo -e "${YELLOW}Success!${NC}"
       echo
@@ -20,116 +23,270 @@ check_command_exec_status () {
   fi
 }
 
-wprehost(){
+# Deletes dump
+deleteDump(){
+  echo "Deleting dump...";
+  rm -f $dbfile
+  check_command_exec_status $?
+}
 
-  # Looking for wp-config.php (WordPress) or .env (Laravel)
+# Imports dump
+importDump(){
+  echo "Importing...";
+  mysql -u$DB_USERNAME -p$DB_PASSWORD $DB_DATABASE < ./$dbfile
+  check_command_exec_status $?
+}
 
+# Searches in DB dump
+SearchInDump(){
+    echo
+    if [ ! -f "$dbfile" ]; then
+        echo -e "${L_RED}No ${dbfile} found!${NC}"
+        return
+    fi
+    read -p 'Search string: ' old_domain
+    echo
+    echo "Searching...";
+    find=`grep -o "$old_domain" "$dbfile" | wc -l | tr -d " "`;
+    check_command_exec_status $?
+    echo -e "Found ${WHITE}$find${NC} occurrences of $old_domain";
+    echo
+}
+
+# Search and replace in dump
+searchReplaceInDump(){
+  SearchInDump
+  echo
+  if [ ! -f "$dbfile" ]; then
+    echo -e "${L_RED}No ${dbfile} found!${NC}"
+    return
+fi
+  read -p 'Replace string: ' new_domain
+  echo
+  echo "Replacing...";
+
+   perl -pi -w -e "s|${old_domain}|${new_domain}|g;" "$dbfile"
+   check_command_exec_status $?
+}
+
+# get DB credentials from config file
+getCredentials(){
+  # Looking for config file
+
+# WordPress
   if [ -f wp-config.php ]; then
-      echo "Found wp-config.php in current directory!"
-
-      # Retriving credentials for DB connection
+       configFile=wp-config.php
+      # Retrieving credentials for DB connection
       CONF=wp-config.php
       DB_DATABASE=`cat "$CONF" | grep DB_NAME | cut -d \' -f 4`
       DB_USERNAME=`cat "$CONF" | grep DB_USER | cut -d \' -f 4`
       DB_PASSWORD=`cat "$CONF" | grep DB_PASSWORD | cut -d \' -f 4`
 
+# Laravel
   elif [[ -f .env ]]; then
-      echo "Found .env in current directory!"
+      configFile=.env
       source .env
   else
-    echo -e "${L_RED}Can't find neither wp-config.php nor .env in current directory... :(${NC}"
-    return
+    DB_DATABASE=''
+    DB_USERNAME=''
+    DB_PASSWORD=''
   fi
 
+}
 
-    echo
-    echo -e "DB name: ${L_GREEN}$DB_DATABASE${NC}"
-    echo -e "DB user: ${L_GREEN}$DB_USERNAME${NC}"
-    echo -e "DB password: ${L_GREEN}$DB_PASSWORD${NC}"
-    echo
-
-read -p 'Export DB? (y/n): ' x
-case $x in
-y)
-
+# Creates DB dump
+createDump(){
   echo
+  if [[ -z $DB_DATABASE ]]; then
+     echo -e "${L_RED}Can't find neither wp-config.php nor .env in current directory${NC}"
+     return
+  fi
   echo "Making DB dump...";
   mysqldump -u$DB_USERNAME -p$DB_PASSWORD $DB_DATABASE > ./$DB_DATABASE.sql
   dbfile="$DB_DATABASE.sql"
   check_command_exec_status $?
+#    This is for dumpStats
+  remote=1
+}
 
-  read -p 'Search in DB dump? (y/n): ' y
-    case $y in
-      y)
+CheckAndSearchInDump(){
+     if [ -f "$dbfile" ]; then
+        SearchInDump
+        else
+            echo -e "${L_RED}No ${dbfile} found!${NC}"
+        return
+    fi
+}
+
+showCredentials(){
+  echo
+  echo -e "DB name: ${WHITE}$DB_DATABASE${NC}"
+  echo -e "DB user: ${WHITE}$DB_USERNAME${NC}"
+  echo -e "DB password: ${WHITE}$DB_PASSWORD${NC}"
+}
+
+askUserNoVariants(){
+read -p "What do you want from me? (1-9, enter for help): " action
+}
+
+showdelimiter(){
         echo
+        echo '-------------------'
         echo
-        read -p 'Search string: ' old_domain
+}
+
+title(){
+    echo -e "${D_VIOL}$1${NC}"
+}
+
+dumpStats(){
         echo
-        echo "Searching...";
+        # Config file
+        if [[ -z $DB_DATABASE ]]; then
+                echo -e "${L_RED}Can't find neither wp-config.php nor .env in current directory${NC}"
+            else
+                echo -e "Config file: ${WHITE}$configFile${NC}"
+        fi
 
-        find=`grep -o "$old_domain" "$dbfile" | wc -l | tr -d " "`;
-        check_command_exec_status $?
+        # DB dump
+        if [ -f "$dbfile" ]; then
+            dumpSize=$(du -k -h $dbfile | cut -f1 | tr -d ' ')
+            dumpChangeDate=$(date -r $dbfile)
+            echo -e "DB dump file: ${WHITE}$dbfile${NC}"
+            echo -e "Remote or local dump: $(remoteOrLocalDump)"
+            echo -e "Size: ${WHITE}$dumpSize${NC}"
+            echo -e "Last changed: ${WHITE}$dumpChangeDate${NC}"
+        else
+        echo -e "${L_RED}No DB dump found!${NC}"
+        fi
+}
 
-        echo "Found $find occurrences of $old_domain";
+# Determines if dump made from local or remote DB
+remoteOrLocalDump(){
+    if [[ $remote -eq 1 ]]; then
+#        Local
+        echo -e "${WHITE}Local${NC}"
+    elif [[ $remote -eq 2 ]]; then
+#       Remote
+        echo -e "${YELLOW}Remote (${remotePath})${NC}"
+     else
+        echo -e "Not sure ..."
+    fi
+}
 
-        echo
-        read -p "Replace ${old_domain} in DB dump? (y/n): " yy
-        case $yy in
-          y)
-          echo
-          echo
-          read -p 'Replace string: ' new_domain
-          echo
-          echo "Replacing...";
+surprise(){
+    curl parrot.live
+#    curl http://artscene.textfiles.com/asciiart/angela.art
+#     curl http://artscene.textfiles.com/asciiart/cow.txt
+}
 
-           perl -pi -w -e "s|${old_domain}|${new_domain}|g;" "$dbfile"
-           check_command_exec_status $?
+PullDumpFromRemote(){
+    echo
+    read -p "Remote host? For example, root@123.45.12.23 or 47: " host
+    echo
+    read -p "Path on remote? For example, /path/to/website: " path
+    echo -e "Creating dump on remote server"
+    echo
+#    Triming trailing slash
+    path=${path%%+(/)}
+#    Creating dump on remote server and echoing only dump name
+    remoteDump=`ssh -t $host "cd $path && $(declare -f getCredentials createDump dumpStats check_command_exec_status); getCredentials; createDump > /dev/null 2>&1 ; dumpStats > /dev/null 2>&1;printf "'$dbfile'`
+    check_command_exec_status
 
-            read -p "Import? (y/n): " yyy
-            case $yyy in
-              y)
-             echo "Importing...";
-             mysql -u$DB_USERNAME -p$DB_PASSWORD $DB_DATABASE < ./$dbfile
-             check_command_exec_status $?
+#    In case $dbfile is not set
+     if [ ! -f "$dbfile" ]; then
+        dbfile=$remoteDump
+    fi
 
-             echo "Deleting dump...";
-             rm -f $dbfile
-             check_command_exec_status $?
-             ;;
-             *)
-             echo "Abort";
-           ;;
-         esac # Import
-          ;;
-          *)
-            echo "Abort";
-          ;;
-        esac # Replace
+#    Pulling dump from remote
+    remotePath="${host}:${path}/${remoteDump}"
+    echo -e "Pulling dump from remote ${remotePath}"
+    scp "${remotePath}" "${dbfile}"
+    check_command_exec_status
+#    This is for dumpStats
+    remote=2
+}
+
+
+askUserWithVariants(){
+echo -e "What do you want from me?
+    ${WHITE}1.${NC} Show Credentials
+    ${WHITE}2.${NC} Export DB locally
+    ${WHITE}3.${NC} Search in dump
+    ${WHITE}4.${NC} Search/Replace in dump
+    ${WHITE}5.${NC} Import dump
+    ${WHITE}6.${NC} Export dump and pull from remote website
+    ${WHITE}7.${NC} Party! Ctrl+C to exit party
+    ${WHITE}9.${NC} Exit"
+read -p "Type number (1-9): " action
+}
+
+###################################################
+# Routing
+###################################################
+doStuff(){
+    case $action in
+    1)
+        title 'showCredentials'
+        showCredentials
         ;;
-      *)
-        echo 'Abort';
+    2)
+        title 'createDump'
+        createDump
+       ;;
+    3)
+        title 'CheckAndSearchInDump'
+        CheckAndSearchInDump
         ;;
-  esac # Search
-;;
-*)
-read -p "Import DB? (y/n): " xy
-case $xy in
-  y)
-    read -p "Dump file name (empty for default name): " name
-      echo "Importing...";
-      if [[ $name = '' ]]; then
-        dbfile="$DB_DATABASE.sql"
-      else
-        dbfile=$name;
-      fi
-      # echo $dbfile;
-      mysql -u$DB_USERNAME -p$DB_PASSWORD $DB_DATABASE < ./$dbfile
-      check_command_exec_status $?
-  ;;
-  *)
-  echo 'Abort';
-  ;;
-esac
-;;
-esac # Export DB? (y/n)
+    4)
+        title 'searchReplaceInDump'
+        searchReplaceInDump
+        ;;
+    5)
+        title 'importDump'
+        importDump
+        ;;
+    6)
+        title 'PullDumpFromRemote'
+        PullDumpFromRemote
+        ;;
+    7)
+        title "Surprise!"
+        surprise
+        ;;
+    9)
+        title 'Bye!'
+        break
+        ;;
+    *)
+#        default
+        title 'Need help?'
+        dumpStats
+        showdelimiter
+        askUserWithVariants
+        showdelimiter
+        doStuff
+        ;;
+    esac
+}
+
+###########################
+
+wprehost(){
+
+getCredentials
+
+showdelimiter
+title "Hello from $SCRIPT_NAME script!"
+
+while :
+    do
+        showdelimiter
+        askUserNoVariants
+        showdelimiter
+        doStuff
+    done
+return
+
+
 } # wprehost()
